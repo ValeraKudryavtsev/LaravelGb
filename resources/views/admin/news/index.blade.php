23c7d3a1a590e9ff7dec57a32bffd758f9a311b2
valera_kudryavtsev <shrhannor@gmail.com>
2023-08-02 17:24:56 +0300

    task4 has been done

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -4,7 +4,7 @@
         <h1 class="h2">Список новостей</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
-                <a type="button" class="btn btn-sm btn-outline-secondary">Добавить запись</a>
+                <a href="{{ route('admin.news.create') }}" class="btn btn-sm btn-outline-secondary">Добавить запись</a>
             </div>
         </div>
     </div>
@@ -23,7 +23,6 @@
             </thead>
            <tbody>
              @forelse($newsList as $news)
-                 @dump($loop->first)
                  <tr>
                      <td>{{ $news['id'] }}</td>
                      <td>{{ $news['title'] }}</td>
